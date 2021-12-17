d3cc3b91eaa7ba1e1ad48fcb96641ee7ff36c6f4
Victor Roberto <Robertovictor0702@gmail.com>
2021-12-17 15:40:49 -0300

    mudanças versão 0.0.2

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Card name="Equilibrium"/>
-      <Card name="Shiba"/>
-      <Card name="Bit Coin"/>
+      <Card name="Equilibrium" id = "#3429" value = "0.041"/>
+
+      <Card name="Shiba" id = "#2892" value = "0.0052"/>
+      
+      <Card name="Bit Coin" id = "#5970" value = "7.241"/>
     </div>
   );
 }
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,4 +1,6 @@
 import equilibrium from '../../images/image-equilibrium.jpg'
+import ethereumIcon from '../../images/icon-ethereum.svg'
+import clock from '../../images/icon-clock.svg'
 
 import './style.css'
 
@@ -8,10 +10,23 @@ function Card(props) {
             <div className='CardImage'>
                     <img src={equilibrium} alt="Equilibrium img"/>
             </div>
+
             <div className='CardText'>
-            <h2>{props.name}</h2>
-            <p>Our {props.name} collection promotes balance and calm</p>
+                <h2>{props.name} {props.id}</h2>
+                <p>Our {props.name} collection promotes balance and calm</p>
             </div>
+
+            <div className='CardInfo'>
+                <img src={ethereumIcon} alt="ethereum"/>
+                <p>{props.value} ETH</p>
+
+                <div className='TimeInfo'>
+                    <img src={clock} alt="clock" />
+                    <p>3 days left</p>
+                </div>
+
+            </div>
+
         </div>
     )
 }
